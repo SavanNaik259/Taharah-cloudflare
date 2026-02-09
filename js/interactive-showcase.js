1f8bed13df61430ce64a6008dcc5ff11defaefe6
Savan <githost.2007@gmail.com>
2026-02-09 04:57:36 +0000

    fk

--- js/interactive-showcase.js
+++ js/interactive-showcase.js
@@ -29,7 +29,7 @@ const InteractiveShowcase = (function () {
     async function setupSampleProducts() {
         console.log("Loading showcase products from Firebase...");
 
-        const showcaseProductIds = ["1770560590161-wfc2evye-868720"];
+        const showcaseProductIds = ["NEW-1770560590161-wfc2evye-868720"];
 
         // Dot classes for positioning on the image
         const dotClasses = ["dot-necklace", "dot-earrings","dot-bangle"];
