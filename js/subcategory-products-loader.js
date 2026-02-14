63d90f251539778215506328c65a361c8bb5a28b
Savan <githost.2007@gmail.com>
2026-02-14 00:56:11 +0000

    Revert "Improve sorting and filtering of products by date"
    This reverts commit c1f1ffe2b65acbee8aab37b92457cbe5a3cded3d.

--- js/subcategory-products-loader.js
+++ js/subcategory-products-loader.js
@@ -289,25 +289,9 @@ const SubcategoryProductsLoader = (function() {
                 
             case 'newest':
                 return productsCopy.sort((a, b) => {
-                    const getTime = (p) => {
-                        if (typeof p.uploadedAt === 'number') return p.uploadedAt;
-                        if (typeof p.createdAt === 'number') return p.createdAt;
-                        if (typeof p.timestamp === 'number') return p.timestamp;
-                        if (typeof p.date === 'number') return p.date;
-
-                        const val = p.uploadedAt || p.createdAt || p.timestamp || p.date;
-                        if (val) {
-                            const date = new Date(val);
-                            const time = date.getTime();
-                            if (!isNaN(time)) return time;
-                        }
-                        
-                        const idMatch = p.id?.match(/-(\d{13})-/);
-                        if (idMatch) return parseInt(idMatch[1]);
-                        
-                        return 0;
-                    };
-                    return getTime(b) - getTime(a);
+                    const dateA = new Date(a.date || 0);
+                    const dateB = new Date(b.date || 0);
+                    return dateB - dateA;
                 });
                 
             case 'featured':
